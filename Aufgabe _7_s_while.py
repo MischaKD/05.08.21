f48ef85280201316af094518350d1724ae4a2978
MischaKD <mkabdulov@gmail.com>
2021-08-06 13:41:03 +0300

    Aufgabe 7_a:

--- Aufgabe _7_s_while.py
+++ Aufgabe _7_s_while.py
@@ -1,17 +1,40 @@
-import datetime
+# Aufgabe 7_c:
+
+# import datetime
+#
+# active = True
+# dt_list = []
+# while active:
+#     ans = input("Enter druecken um Zeit zu speichern, 'o' zum Beenden: ")
+#     if ans == "0":
+#         active = False
+#     now1 = datetime.datetime.now()
+#     dt_list.append(now1)
+#     # print(now1, type(now1))
+# print("Eingabezeiten")
+# for i in dt_list:
+#     print(i.strftime("%X"))
+
+# Aufgabe 7_a:
 
 active = True
-dt_list = []
+myDict = {}
 while active:
-    ans = input("Enter druecken um Zeit zu speichern, 'o' zum Beenden: ")
-    if ans == "0":
-        active = False
-    now1 = datetime.datetime.now()
-    dt_list.append(now1)
-    # print(now1, type(now1))
-print("Eingabezeiten")
-for i in dt_list:
-    print(i.strftime("%X"))
+      name = input("Please enter your name: name 0 will terminate: ")
+      if name == "0":
+          active = False
+          break
+      age = int(input("Please enter you age: "))
+      myDict[name] = age
+
+print(f"{'Name':>10} {'age':<10}:")
+for item, amount in myDict.items():  # dct.iteritems() in Python 2
+    print(f"{item:>10} {amount:<10}")
+
+
+
+
+
 
 
 
