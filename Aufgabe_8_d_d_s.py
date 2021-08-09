72f0b714b5e6ccc0134c6f0a397f9ed159535223
MischaKD <mkabdulov@gmail.com>
2021-08-09 16:09:59 +0300

    Aufgabe 8: #Aufgabe_8_d_d_s Import from Moduls

---
+++ Aufgabe_8_d_d_s.py
@@ -0,0 +1,16 @@
+import Aufgabe_8_a_s
+#
+# res = Aufgabe_8_a_s.myfunc()
+# print(res)
+
+# from Aufgabe_8_a_s import myfunc
+# # res = myfunc()
+# # print(res)
+# myfunc()
+
+# Aufgabe_8_a_s.myfunc()
+res = Aufgabe_8_a_s.myfunc()
+print(res)
+
+res1 = Aufgabe_8_a_s.conftime(res)  # conftime()
+print(res1)
