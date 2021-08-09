9b73b69c0d54415c74a1b44960f7ba1063a23fbe
MischaKD <mkabdulov@gmail.com>
2021-08-09 15:33:15 +0300

    Aufgabe 8: #Aufgabe_8_c_s

--- Aufgabe_8_a_s.py
+++ Aufgabe_8_a_s.py
@@ -19,12 +19,47 @@ def myfunc():
         age = int(input("Please enter you age: "))
         myDict[name] = age
 
-    print(f"{'Name':<15} {'age':<5}")
-    for item, amount in myDict.items():
-        print(f"{item:<15} {amount:<5}")
+    # print(f"{'Name':<15} {'age':<5}")
+    # for item, amount in myDict.items():
+    #     print(f"{item:<15} {amount:<5}")
 
     return myDict
 
 res = myfunc()
 print(res)
 
+
+#Aufgabe_8_c_s
+
+print("Aufgabe c:")
+from datetime import date, time, datetime                  # datetime importieren
+
+def conftime():
+    my_Confirmed_Dict = {}                                     # leeres Dictionary erzeugen
+    for name,age in res.items():                            # myDict aus vorheriger Aufgabe
+        print (name,age)                                           # Ausgabe der Eintraege
+    #    print(age)                                             # Ausgabe der Eintraege
+        ans = input("Confirm User? (y)")                       # Abfrage zur Bestaetigung
+        if ans == "y":
+    #        my_Confirmed_Dict[name]=age
+    # get time entry
+            today = date.today()                               # Zeitangabe erzeugen
+            now = datetime.now()
+            current_time = time(now.hour,now.minute, now.second)
+            combined_time = datetime.combine(today, current_time)
+    #        print(combined_time,type(combined_time))
+    # create new dict for every key (user)
+            new_dict = {}                                      # neues Dictionary fuer
+            new_dict["age"] = age                              # bestaetigte User anlegen
+            new_dict["time"] = combined_time                   # Umwandlung von datetime objekt in string mit str()
+            my_Confirmed_Dict[name] = new_dict
+
+    #print(my_Confirmed_Dict)
+
+    msg = f"     {'User':10s}       {'age':3}                                 {'date'}  "
+    print(msg)
+    for key,item in my_Confirmed_Dict.items():
+         var = item["time"].strftime("%A, %B %Y %H-%M-%S")
+         print(f"User {key:10s} is    {item['age']:3d}  years old and was confirmed at {var}. ")
+
+conftime()
