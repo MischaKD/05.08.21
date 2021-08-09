e1a216335a29fc8f85248201c2971722839d2c63
MischaKD <mkabdulov@gmail.com>
2021-08-09 14:33:02 +0300

    Aufgabe 8: #Aufgabe_8_b_s

--- Aufgabe_8_a_s.py
+++ Aufgabe_8_a_s.py
@@ -1,5 +1,30 @@
-def myfunc(x, y):
-    a = x + y
-    return a
 
-print("The result ist " + str(myfunc(5,6)))
+#Aufgabe_8_a_s
+# def myfunc(x, y):
+#     a = x + y
+#     return a
+#
+# print("The result ist " + str(myfunc(5,6)))
+
+#Aufgabe_8_b_s
+
+def myfunc():
+    active = True
+    myDict = {}
+    while active:
+        name = input("Please enter your name: name 0 will terminate: ")
+        if name == "0":
+            active = False
+            break
+        age = int(input("Please enter you age: "))
+        myDict[name] = age
+
+    print(f"{'Name':<15} {'age':<5}")
+    for item, amount in myDict.items():
+        print(f"{item:<15} {amount:<5}")
+
+    return myDict
+
+res = myfunc()
+print(res)
+
