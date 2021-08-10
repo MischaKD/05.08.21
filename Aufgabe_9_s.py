123416e77cf3fb2b306f6bf2d16efca2163f50e4
MischaKD <mkabdulov@gmail.com>
2021-08-10 13:02:04 +0300

    Aufgabe 9: Class #Aufgabe_9_b_s  add new method

--- Aufgabe_9_s.py
+++ Aufgabe_9_s.py
@@ -1,12 +1,36 @@
-#Aufgabe_9_a_s
+# #Aufgabe_9_a_s
+# class Person():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def pri(self):
+#         print(f"My name is {self.name}, and I am {self.age} year old:")
+#         # print(self.name)
+#
+# new_person = Person("Ivan", "19")
+# new_person.pri()
+
+
+#Aufgabe_9_b_s
 class Person():
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def pri(self):
-        print(f"My name is {self.name}, and I am {self.age} year old:")
+        print(f"My name is {self.name}, and I am {self.age} year old!")
         # print(self.name)
 
-new_person = Person("Ivan", "19")
+    def new_data(self, new_name, new_age):
+        self.name = new_name
+        self.age = new_age
+        print(f"Another is {self.name}, and the new age is {self.age}!")
+
+
+new_person = Person("Ivan", 19)
 new_person.pri()
+
+new_person.new_data("Roma", 20)
+# print(new_person.name)
+# print(new_person.age)
