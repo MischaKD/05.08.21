88e883d105d293306f3311500509e366c1559366
MischaKD <mkabdulov@gmail.com>
2021-08-16 09:45:45 +0300

    Kapitel 13: json

--- Aufgabe_10_a_s.py
+++ Aufgabe_10_a_s.py
@@ -1,21 +1,22 @@
 # Variant 1 with results in Terminal
 
-dateiname = "aufgabe10_a.txt"
-a = 1
-b = 1
-with open(dateiname,"w") as f:
-    for i in range(1,101):
-        if i < 101:
-            d = f"{i:.1f}, {a:.1f}, {b:.1f}\n"
-            res = f.write(d)  # Zeilenweises Schreiben am Dateiende (Zeile wird angehaengt)
-            print(d, end="")# res = f.write(f"{a},")
-            # res = f.write(f"{b}\n")
-            # res += f.write("\n")
-            a +=2
-            b +=3
-
-print(dateiname)
-print(f)
+# dateiname = "aufgabe10_a.txt"
+# a = 1
+# b = 1
+# with open(dateiname,"w") as f:
+#     for i in range(1,101):
+#         if i < 101:
+#             d = f"{i:.1f}, {a:.1f}, {b:.1f}\n"
+#             res = f.write(d)  # Zeilenweises Schreiben am Dateiende (Zeile wird angehaengt)
+#             print(d, end="")
+#             # res = f.write(f"{a},")
+#             # res = f.write(f"{b}\n")
+#             # res += f.write("\n")
+#             a +=2
+#             b +=3
+#
+# print(dateiname)
+# print(f)
 
 # Variant 2 without result in Terminal
 
@@ -29,6 +30,6 @@ with open(dateiname,"w") as f:
             a +=2
             b +=3
 
-print(dateiname)
-print(f)
+# print(dateiname)
+# print(f)
 
--- Aufgabe_10_b_s.py
+++ Aufgabe_10_b_s.py
@@ -1,16 +1,25 @@
 # Variant 1 without reading
-dateiname = "aufgabe10_b.txt"
-a = 1
-b = 1
-with open(dateiname,"w") as f:
-    for i in range(1,101):
-        if i < 101:
-            c = i + a + b
-            res = f.write(f"{i:.1f}, {a:.1f}, {b:.1f}, {c:.1f}\n")  # Zeilenweises Schreiben am Dateiende (Zeile wird angehaengt)
-            a +=2
-            b +=3
-
-print(dateiname)
-print(f)
+# dateiname = "aufgabe10_b.txt"
+# a = 1
+# b = 1
+# with open(dateiname,"w") as f:
+#     for i in range(1,101):
+#         if i < 101:
+#             c = i + a + b
+#             res = f.write(f"{i:.1f}, {a:.1f}, {b:.1f}, {c:.1f}\n")  # Zeilenweises Schreiben am Dateiende (Zeile wird angehaengt)
+#             a +=2
+#             b +=3
+#
+# print(dateiname)
+# print(f)
 
 # Variant 2 with reading
+
+with open("aufgabe10_a.txt","r") as f:
+    mylist = []
+    for line in f:
+        mylist.append(line)
+    for i in mylist:
+        print(i)
+
+print(mylist) # Liste jetzt vollständig.
